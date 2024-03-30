40237ecb23e2c76f128b635580f1619337f0cb58
Zarif Hosen <zarifhosen2015@gmail.com>
2024-03-31 00:47:33 +0600

    unknown

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,28 @@
+import Card from "@/components/card";
+
 export default function Home() {
+  const products = [
+    {
+      id: 1,
+      name: "Product 1",
+      description: "Description 1",
+      price: 10,
+      image: "https://picsum.photos/200/300",
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      description: "Description 2",
+      price: 20,
+      image: "https://picsum.photos/200/300",
+    },
+  ];
+
   return (
-    <main className="min-h-screen mx-auto max-w-[100rem] px-4 sm:px-6 lg:px-8"></main>
+    <main className="min-h-screen mx-auto max-w-[100rem] px-4 sm:px-6 lg:px-8">
+      {products.map((product) => (
+        <Card key={`${product.name}-${product.id}`} {...product} />
+      ))}
+    </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,6 +20,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("@tailwindcss/line-clamp")],
+  plugins: [],
 };
 export default config;
